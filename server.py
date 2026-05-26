5ac79f7636633fecb114c2adee125765786c1954
void30tiga-code <void30tiga@gmail.com>
2026-05-26 22:42:23 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,17 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 import os
-import requests
 
 app = FastAPI()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-TELEGRAM_API = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
 @app.get("/")
 def home():
-    return {"status": "connected"}
+    return {
+        "status": "online",
+        "token": "detected" if BOT_TOKEN else "missing"
+    }
 
-@app.post("/webhook")
-async def webhook(req: Request):
-    data = await req.json()
-
-    if "message" in data:
-        chat_id = data["message"]["chat"]["id"]
-
-        text = "✅ Bot aktif!\nKirim foto untuk diproses."
-
-        requests.post(
-            f"{TELEGRAM_API}/sendMessage",
-            json={
-                "chat_id": chat_id,
-                "text": text
-            }
-        )
-
-    return {"ok": True}
+@app.get("/webhook")
+def webhook():
+    return {"message": "ready"}
